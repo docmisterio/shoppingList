e412a6602caff725d64d1b8fff20c13e23e5e9cc
Zach Becker <doc.misterio@gmail.com>
2020-11-14 16:20:20 -0600

    finished the project but I'm adding some features that I want

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -6,13 +6,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "Shopping List"
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addToList))
-        
-        
-        shoppingListItems = []
+        setUpTitle()
+        setupRightBarButtonItem()
+        setupLeftBarButtonItem()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -30,14 +26,43 @@ class ViewController: UITableViewController {
         ac.addTextField()
         
         let submit = UIAlertAction(title: "Add", style: .default) {
-            [weak self, weak ac] things in
+            [weak self, weak ac] _ in
             guard let item = ac?.textFields?[0].text else { return }
+            self?.submitItem(newItem: item)
         }
         
         ac.addAction(submit)
         
         present(ac, animated: true)
         print(shoppingListItems)
+    }
+    
+    func submitItem(newItem: String) {
+        if newItem.isEmpty { return }
+        shoppingListItems.insert(newItem, at: 0)
+        
+        let placement = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [placement], with: .automatic)
+    }
+    
+    func setUpTitle() {
+        let shoppingListTitle = "Shopping List"
+        title = shoppingListTitle
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    @objc func deleteList() {
+        shoppingListItems = []
+        let sections = [0]
+        tableView.reloadSections(IndexSet(sections), with: .automatic)
+    }
+    
+    func setupRightBarButtonItem() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addToList))
+    }
+    
+    func setupLeftBarButtonItem() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteList))
     }
 }
 
